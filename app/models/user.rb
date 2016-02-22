7a495658d758fc00b036d02328ff284b08a05fe9
Pablo Fonseca <pablofonseca777@gmail.com>
2016-02-22 19:29:45 -0300

    Add authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_auth_token!
+
   validates_presence_of :first_name, :last_name
+  validates :auth_token, uniqueness: true
+
+  def generate_auth_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:auth_token) }
 
   it { should be_valid }
 
@@ -20,5 +21,20 @@ describe User do
     it { should validate_confirmation_of(:password) }
     it { should allow_value('example@domain.com').for(:email) }
     it { should_not allow_value('exampledomain.com').for(:email) }
+    it { should validate_uniqueness_of(:auth_token)}
+  end
+
+  describe '#generate_auth_token!' do
+    it 'generates an unique token' do
+      allow(Devise).to receive(:friendly_token).and_return('uniquetoken001')
+      @user.generate_auth_token!
+      expect(@user.auth_token).to eq 'uniquetoken001'
+    end
+
+    it 'generates another token when one already has been taken' do
+      existent_user = create(:user, auth_token: "uniquetoken001")
+      @user.generate_auth_token!
+      expect(@user.auth_token).not_to eql existent_user.auth_token
+    end
   end
 end
